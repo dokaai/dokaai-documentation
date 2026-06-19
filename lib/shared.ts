13765df74c7a4ef7aae19fabec05575d357dd725
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2026-06-19 16:56:09 +0530

    feat: #001: Fixed bugs and added ico

--- lib/shared.ts
+++ lib/shared.ts
@@ -28,7 +28,7 @@ export const docsContentRoute = withBasePath('/llms.mdx');
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'dokaai',
+  repo: 'dokaai-documentation',
   branch: 'main',
 };
